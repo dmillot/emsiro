7fda06543005a899994d72512084cc6b8bd29d78
Damien Millot <Damien.Millot@diiage.org>
2019-11-09 19:12:47 +0100

    edit index

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// const fetch = require('node-fetch');
+// fetch('http://vps.cours-diiage.com:8080', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//     },
+//     body: JSON.stringify({query: "{poi { total results { rdf_type }}}"})
+//   })
+//     .then(r => r.json())
+//     .then(data=> console.log(data));
