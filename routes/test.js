b46d0a9d9b8d70ef3823c6c22aac98b88b366f62
charly darceaux <charly.darceaux@diiage.org>
2019-11-12 17:54:06 +0100

    test api

--- routes/test.js
+++ routes/test.js
@@ -1,124 +1,49 @@
 var express = require("express");
 var router = express.Router();
-var https = require("https");
 var fetch = require("node-fetch");
 
 // Call API
 
-router.get("/test/:activityId", function(req, res, next) {
+router.get("/", function(req, res, next) {
   console.log();
   var dataString = JSON.stringify({
     query:
       `
       {
-        poi(
-          filters: [{rdf_type: {_in: ["https://www.datatourisme.gouv.fr/ontology/core#`+ req.param.activityId +`"]}}]
-        ) {
-          results {
-            _uri
-            rdfs_label {
+        poi 
+        { 
+        results {
+          _uri,
+          rdfs_label {
+            value
+          },
+          takesPlaceAt{
+            startDate,
+            endDate
+          },
+          isLocatedAt {
+            schema_address {
+              schema_streetAddress,
+              schema_postalCode,
+              schema_addressLocality
+            },
+            schema_geo{
+              schema_latitude schema_longitude
+            }
+          },
+          hasDescription {
+            shortDescription {
+              value,
+              lang
+            },
+            dc_description {
+              lang,
               value
-            }
-            dc_identifier
-            isLocatedAt {
-              schema_address {
-                schema_streetAddress
-                schema_postalCode
-                schema_addressLocality
-              }
-              schema_openingHoursSpecification {
-                schema_opens
-                schema_closes
-                schema_dayOfWeek {
-                  rdfs_label {
-                    value
-                  }
-                }
-              }
-              schema_geo {
-                schema_latitude
-                schema_longitude
-              }
-            }
-            hasContact {
-              schema_email
-              schema_telephone
-              schema_faxNumber
-              schema_legalName
-            }
-            hasBookingContact {
-              schema_logo
-              schema_email
-              schema_telephone
-              schema_faxNumber
-              schema_legalName
-            }
-            isEquippedWith {
-              rdfs_label {
-                value
-              }
-            }
-            hasFeature {
-              internetAccess
-              airConditioning
-              petsAllowed
-              charged
-              noSmoking
-            }
-            hasMainRepresentation {
-              ebucore_hasRelatedResource {
-                ebucore_locator
-              }
-            }
-            rdf_type
-            hasDescription {
-              shortDescription {
-                value
-                lang
-              }
-              dc_description {
-                lang
-                value
-              }
-            }
-            offers {
-              schema_priceSpecification {
-                schema_priceCurrency
-                hasPricingMode {
-                  rdfs_label {
-                    lang
-                    value
-                  }
-                }
-                hasEligiblePolicy {
-                  rdfs_label {
-                    value
-                  }
-                }
-                hasEligibleAudience {
-                  rdfs_label {
-                    value
-                  }
-                  schema_requiredGender {
-                    rdfs_label {
-                      value
-                    }
-                  }
-                  rdfs_label {
-                    value
-                  }
-                }
-                schema_price
-              }
-              schema_acceptedPaymentMethod {
-                rdfs_label {
-                  value
-                }
-              }
             }
           }
         }
-      }`
+      }
+    }`
   });
   fetch("http://vps.cours-diiage.com:8080", {
     method: "POST",
@@ -130,7 +55,7 @@ router.get("/test/:activityId", function(req, res, next) {
   })
     .then(r => r.json())
     .then(data => {
-      console.log(data), res.render("test", { data: data.data.poi.results[0] });
+    res.render("test", { data: data.data.poi.results });
     });
 });
 
