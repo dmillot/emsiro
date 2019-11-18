adb8de578bd7eef164231489e2a8e921c1e0bbd9
Damien Millot <Damien.Millot@diiage.org>
2019-11-18 13:54:05 +0100

    refactoring disconnect route

--- routes/disconnect.js
+++ routes/disconnect.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var https = require('https');
-var fetch = require('node-fetch');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    req.session.userId= null;
-  res.redirect('/');
-
+router.get('/', function (req, res, next) {
+    req.session.userId = null;
+    res.redirect('/');
 });
 
 module.exports = router;
