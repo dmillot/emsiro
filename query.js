232ac6252f052530325f589469995af650f20341
Damien Millot <Damien.Millot@diiage.org>
2019-11-21 08:38:35 +0100

    add comments

--- query.js
+++ query.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var query = function querySQL(query_sql, db_connection, parameters = null) {
+
+    // create new promise that will be return
     return new Promise((resolve, reject) => {
         db_connection.query(query_sql, parameters, (err, results) => {
-            if (err)
+            if (err) {
+                // if error for the query, return error
                 return reject(err);
+            }
+
+            // if query succeeds, return rows
             resolve(results);
         });
     });
--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -52,21 +52,24 @@ router.post('/login', function (req, res, next) {
     var queryUserExists = 'SELECT confirmInscription FROM user WHERE id = ? LIMIT 1';
     var queryCheckUserPassword = 'SELECT id, password FROM user WHERE id = ? LIMIT 1';
 
+    // create new promise with the query, the database connection and the username sent with the form
     const promise = query(queryFindUser, conn, [req.body.username]);
 
     promise
-        .then(rows => {
+        .then(rows => { // .then() is a method of processing the result after the operation is completed 
             if (rows.length > 0) {
                 userId = rows[0].id;
                 return query(queryUserExists, conn, [userId]);
             }
 
+            // if no user found with the username sent
             throw "This user doesn't exist.";
         })
         .then(rows => {
             if (rows[0].confirmInscription)
                 return query(queryCheckUserPassword, conn, [userId]);
 
+            // if the user registration isn't validated
             throw "Your registration has not been validated by an administrator.";
         })
         .then(rows => {
@@ -75,6 +78,7 @@ router.post('/login', function (req, res, next) {
                 return res.redirect('/admin');
             }
 
+            // if the password is incorrect
             throw "Incorrect username or password.";
         })
         .catch(err => {
